99eec7a6d695298367d2a30c5cf7b83b03cee350
Pavel Prichodko <prichodko.p@gmail.com>
2018-04-09 01:38:32 +0200

    Add React, Next.js, s-c to repos

--- repos.js
+++ repos.js
@@ -1,1 +1,20 @@
-module.exports = []
+module.exports = [
+  {
+    name: 'React',
+    url: 'https://github.com/facebook/react',
+    emoji: ':react:',
+    color: '#60dafb'
+  },
+  {
+    name: 'Next.js',
+    url: 'https://github.com/zeit/next.js',
+    emoji: ':heavy_multiplication_x:',
+    color: '#020202'
+  },
+  {
+    name: 'styled-components',
+    url: 'https://github.com/styled-components/styled-components',
+    emoji: ':nail_care:',
+    color: '#d3758d'
+  }
+]
